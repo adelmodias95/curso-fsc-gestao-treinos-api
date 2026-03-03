96c24a6289b0e75bdbc44b25ccbdfbe71bb49168
Adelmo Dias <adelmo.dias@mzgroup.com>
2026-03-03 11:01:32 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-// const name = "";
-// const age = 18;
-console.log("Hello World!");
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async () => {
+  return { hello: "world" };
+});
+
+fastify.listen({ port: Number(process.env.PORT) ?? 3000 }, function (err) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
