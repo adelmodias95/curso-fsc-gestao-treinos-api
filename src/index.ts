9c594d729d400d3772af870915857c4a4aa08c9c
Adelmo Dias <adelmo.dias@mzgroup.com>
2026-03-03 11:10:19 -0300

    chore: add zod fastify-type-provider-zod setup

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,39 @@
 import Fastify from "fastify";
+import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
 
 const fastify = Fastify({
   logger: true,
 });
 
-fastify.get("/", async () => {
-  return { hello: "world" };
+// Add schema validator and serializer
+fastify.setValidatorCompiler(validatorCompiler);
+fastify.setSerializerCompiler(serializerCompiler);
+
+// fastify.get("/", async () => {
+//   return { hello: "world" };
+// });
+
+fastify.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/",
+  schema: {
+    description: "Hello World",
+    tags: ["Hello World"],
+    response: {
+      200: z.object({
+        message: z.string(),
+      }),
+    },
+  },
+  handler: () => {
+    return {
+      message: "Hello World",
+    };
+  },
 });
 
-fastify.listen({ port: Number(process.env.PORT) ?? 3000 }, function (err) {
+fastify.listen({ port: 3000 }, function (err) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
